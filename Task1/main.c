2777488950027b2e516d6a2c00608f848cba6d9c
noexist1 <a.vorontsova1@g.nsu.ru>
2022-02-16 22:35:30 +0300

    task1

---
+++ Task1/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include <malloc.h>
+#define N 10000000
+
+
+int main() {
+    double *arr = (double *)malloc(N * sizeof(double ));
+    double total = 0;
+#pragma acc data copyin(arr) copyout(total)
+    {
+#pragma acc kernels
+        {
+            for (long int i = 0; i < N; ++i) {
+                arr[i] = sin((2 * M_PI) / N * i);
+                total += arr[i];
+            }
+
+        }
+
+        printf("%e", total);
+        return 0;
+    }
+}
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/мвидео461/OneDrive/Документы/теорема параллелизма
+# Install script for directory: C:/Users/мвидео461/OneDrive/Документы/теория параллелизма
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/мвидео461/OneDrive/Документы/теорема параллелизма/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/мвидео461/OneDrive/Документы/теория параллелизма/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
